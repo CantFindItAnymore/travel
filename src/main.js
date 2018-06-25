397c93fce044c47fa84d22856205342848fda905
ranjun <1069811427@qq.com>
2018-06-26 04:16:36 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 // 解决一像素边框问题
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header_left">返回</div>
-    <div class="header_input">搜索</div>
-    <div class="header_right">选择</div>
+    <div class="header_left">
+      <span class="iconfont">&#xe697;</span>
+    </div>
+    <div class="header_input">
+      <span class="iconfont">&#xe693;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header_right">
+      城市
+      <span class="iconfont arrow_icon">&#xe6dc;</span>
+    </div>
   </div>
 </template>
 
@@ -13,9 +21,10 @@ export default {
 </script>
 
 <style lang='stylus' scoped>
+  @import '~@/assets/styles/varibles'
   .header
-    background-color: skyblue
-    color: black
+    background-color: $bgColor
+    color: white
     display: flex
     line-height: .86rem
     .header_left
@@ -25,6 +34,7 @@ export default {
     .header_input
       flex: 1
       color: #B6B8BB
+      padding-left: .2rem
       height:.64rem
       line-height: .64rem
       background-color: white
@@ -35,4 +45,6 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      .arrow_icon
+        font-size: .2rem
 </style>
