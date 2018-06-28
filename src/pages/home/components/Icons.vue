c653e402de6a9568d505c73deb21e903afd212a2
ranjun <1069811427@qq.com>
2018-06-28 21:09:15 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <!-- slides -->
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div
+          class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.iconUrl"/>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: 1,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: 2,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+          desc: '水上乐园'
+        },
+        {
+          id: 3,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '必游榜单'
+        },
+        {
+          id: 4,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1804/95/8d02011d149bdb02.png',
+          desc: '成都定制游'
+        },
+        {
+          id: 5,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/97/02f5043b51b2102.png',
+          desc: '情迷九寨'
+        },
+        {
+          id: 6,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc: '故宫'
+        },
+        {
+          id: 7,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+          desc: '主题乐园'
+        },
+        {
+          id: 8,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+          desc: '夏日玩水'
+        },
+        {
+          id: 9,
+          iconUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        }
+      ]
+    }
+  },
+  // 将一维数组转换为二维数组
+  computed: {
+    pages () {
+      let pages = []
+      this.iconList.forEach((item, index) => {
+        let page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles'
+  @import '~@/assets/styles/mixins'
+  .icons >>> .swiper-container
+    padding-bottom: 50%
+    height: 0
+  .icons
+    overflow: hidden
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      height: 0
+      overflow: hidden
+      width:25%
+      float: left
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        left: 0
+        right: 0
+        top: 0
+        bottom: .44rem
+        .icon-img-content
+          height: 100%
+          display: block
+          margin: 0 auto
+      .icon-desc
+        position: absolute
+        bottom: 0
+        left: 0
+        right: 0
+        height: .44rem
+        line-height: .44rem
+        color: $darkTextColor
+        text-align: center
+        ellipsis()
+</style>
