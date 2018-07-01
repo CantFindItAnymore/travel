e13c8f4559bcfbdc3dd44f476be64be9910081cc
ranjun <1069811427@qq.com>
2018-07-02 06:42:22 +0800

    add citylist and cityalphabet

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="alphabet">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+    <li class="item">D</li>
+    <li class="item">E</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang='stylus' scoped>
+  .alphabet
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    right: 0
+    top: 1.58rem
+    bottom: 0
+    width: .4rem
+    text-align: center
+    background: yellow
+    .item
+      height: .4rem
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="list" ref="list">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">
+          当前城市
+        </div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">
+          热门城市
+        </div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              沉睡谷
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">
+          A
+        </div>
+        <div class="city-list">
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">
+          A
+        </div>
+        <div class="city-list">
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">
+          A
+        </div>
+        <div class="city-list">
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+          <div class="city border-bottom">
+            风电场
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.list)
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .list
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    overflow: hidden
+  .title
+    height: .64rem
+    line-height: .64rem
+    font-size: .26rem
+    background: #eee
+    color: #666
+    padding-left: .2rem
+  .button-list
+    padding: .1rem .6rem .1rem .1rem
+    overflow: hidden
+    .button-wrapper
+      float: left
+      width: 33.33%
+    .button
+      margin: .1rem
+      padding: .1rem
+      text-align: center
+      border: .01rem solid #ccc
+      border-radius: .06rem
+  .city-list
+    .city-wrapper
+    .city
+      height: .76rem
+      line-height: .76rem
+      padding-left: .2rem
+</style>
