ead260a203ec960df8de040f6bde655c4e3efe78
ranjun <1069811427@qq.com>
2018-07-05 07:27:20 +0800

    add detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,56 @@
   <div>
     <DetailHeader/>
     <DetailBanner/>
-    <div class="conte"></div>
+    <div class="conte">
+      <DetailList :list="list"/>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailHeader,
-    DetailBanner
+    DetailBanner,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人票11111',
+              children: [
+                {title: '成人票11111----1111'},
+                {title: '成人票11111----2222'},
+                {title: '成人票11111----3333'}
+              ]
+            },
+            {
+              title: '成人票22222'
+            },
+            {
+              title: '成人票33333'
+            }
+          ]
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        },
+        {
+          title: '学生票'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>
+      <div
+        v-for="(item, index) of this.list"
+        :key="index"
+      >
+        <div class="list-title border-bottom">
+          <span class="list-title-icon"></span>
+          {{item.title}}
+        </div>
+        <!-- 递归组件 -->
+        <div class="list-children" v-if="item.children">
+          <DetailList :list="item.children"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~@/assets/styles/varibles'
+  .list-title
+    height: .86rem
+    line-height: .86rem
+    font-size: .32rem
+    padding-left: .1rem
+    .list-title-icon
+      display: inline-block
+      width: .36rem
+      height: .36rem
+      background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      margin-right: .1rem
+      background-size: .4rem 3rem
+  .list-children
+    padding-left: .24rem
+</style>
